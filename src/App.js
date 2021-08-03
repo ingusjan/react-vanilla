6a8b0db54c9882dd9cec01a6aa8940657df753c7
Ingus <ingusjan@gmail.com>
2021-08-03 14:25:30 +0100

    fix: add .babelrc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import MyComponent from "react-library";
 
 const App = () => {
   return (
     <>
       <h1>React App</h1>
+      <p>This is a simple app</p>
     </>
   );
 };
